cc3f42d5c50339a5e717c7c477461dc6954f3afe
Elijah-Kantola <s-elijah.kantola@lwtech.edu>
2026-06-17 20:13:22 -0700

    Add files via upload
    Added proper boundary testing

--- Final.java
+++ Final.java
@@ -1,3 +1,7 @@
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class Final {
 
     public static boolean canDrive(int age) {
@@ -6,28 +10,53 @@ public class Final {
     }
 
     @Test
-    public void TestBelowAge() {
+    public void testBelowAge() {
         assertFalse(Final.canDrive(15));
     }
 
     @Test
-    public void TestAtAge() {
+    public void testAtAge() {
         assertTrue(Final.canDrive(16));
     }
 
     @Test
-    public void TestAboveAge() {
+    public void testAboveAge() {
         assertTrue(Final.canDrive(17));
     }
 
     @Test
-    public void TestZero() {
+    public void testNegative() {
+        assertFalse(Final.canDrive(-1));
+    }
+
+    @Test
+    public void testZero() {
         assertFalse(Final.canDrive(0));
     }
 
     @Test
-    public void TestMaxValue() {
+    public void testAboveZero() {
+        assertfalse(Final.canDrive(1));
+    }
+
+    @Test
+    public void belowMaxValue() {
+        assertTrue(Final.canDrive(Integer.MAX_VALUE - 1));
+    }
+
+    @Test
+    public void testMaxValue() {
         assertTrue(Final.canDrive(Integer.MAX_VALUE));
+    }
+
+    @Test
+    public void testMinValue() {
+        assertFalse(Final.canDrive(Integer.MIN_VALUE));
+    }
+
+    @Test
+    public void testJustAboveMinValue() {
+        assertFalse(Final.canDrive(Integer.MIN_VALUE + 1));
     }
 }
 
